06f634197707dda9347a838bf85c0b80316d66fa
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-01-28 11:05:49 +0530

    Update Google_Paid_Login.java

--- src/test/java/StepDefination/Google_Paid_Login.java
+++ src/test/java/StepDefination/Google_Paid_Login.java
@@ -68,7 +68,7 @@ public class Google_Paid_Login extends BaseClass {
 					WebElement g_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
 							"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")));
 					Thread.sleep(3000);
-					g_email.sendKeys("slidetech.qa@gmail.com");
+					g_email.sendKeys("nisha.dhiman@slidetech.in");
 					Thread.sleep(3000);
 
 					WebElement g_login_btn1 = wait.until(ExpectedConditions
@@ -81,7 +81,7 @@ public class Google_Paid_Login extends BaseClass {
 					WebElement g_pass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
 							"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")));
 					Thread.sleep(3000);
-					g_pass.sendKeys("Himanshi@123");
+					g_pass.sendKeys("Nisha@123");
 
 					Thread.sleep(3000);
 					WebElement g_login_btn2 = wait.until(ExpectedConditions
