1266ad9df6844788c463a6d8df61b2891df8d2b0
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-01-28 13:45:52 +0530

    Update SLI_search_google_paid_login.java

--- src/test/java/StepDefination/SLI_search_google_paid_login.java
+++ src/test/java/StepDefination/SLI_search_google_paid_login.java
@@ -62,61 +62,61 @@ public class SLI_search_google_paid_login extends BaseClass {
 
 	@Then("user enters the username and password_viii$")
 	public void user_enters_the_username_and_password_viii() throws Throwable {
-		
-			driver.manage().window().maximize();
 
-			// Store the CurrentWindow for future reference
-			// String handle = " ";
-			String currentWindow = driver.getWindowHandle();
-			String popupWindowHandle = null;
+		driver.manage().window().maximize();
 
-			// Switch To Popup Window
+		// Store the CurrentWindow for future reference
+		// String handle = " ";
+		String currentWindow = driver.getWindowHandle();
+		String popupWindowHandle = null;
 
-			for (String handle : driver.getWindowHandles()) {
-				if (!handle.equals(currentWindow)) {
+		// Switch To Popup Window
 
-					popupWindowHandle = handle;
-					driver.switchTo().window(popupWindowHandle);
-					driver.manage().window().maximize();
+		for (String handle : driver.getWindowHandles()) {
+			if (!handle.equals(currentWindow)) {
 
-					Thread.sleep(5000);
+				popupWindowHandle = handle;
+				driver.switchTo().window(popupWindowHandle);
+				driver.manage().window().maximize();
 
-					if (!driver.findElements(By.xpath("//div[@class='BHzsHc']")).isEmpty()) {
-						WebElement another_btn = BaseClass
-								.elementToBeClickable(By.xpath("//div[text()='Use another account']"));
-						another_btn.click();
-					}
+				Thread.sleep(5000);
 
-					WebElement g_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
-							"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")));
-					Thread.sleep(3000);
-					g_email.sendKeys("nisha.dhiman@slidetech.in");
-					Thread.sleep(3000);
-
-					WebElement g_login_btn1 = wait.until(ExpectedConditions
-							.elementToBeClickable(By.cssSelector("#identifierNext > div > button > span")));
-					Thread.sleep(3000);
-					g_login_btn1.click();
-
-					Thread.sleep(3000);
-
-					WebElement g_pass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
-							"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")));
-					Thread.sleep(3000);
-					g_pass.sendKeys("Nisha@123");
-
-					Thread.sleep(3000);
-					WebElement g_login_btn2 = wait.until(ExpectedConditions
-							.elementToBeClickable(By.cssSelector("#passwordNext > div > button > span")));
-					Thread.sleep(3000);
-					g_login_btn2.click();
-
-					Thread.sleep(5000);
+				if (!driver.findElements(By.xpath("//div[@class='BHzsHc']")).isEmpty()) {
+					WebElement another_btn = BaseClass
+							.elementToBeClickable(By.xpath("//div[text()='Use another account']"));
+					another_btn.click();
 				}
+
+				WebElement g_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+						"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")));
+
+				g_email.sendKeys("nisha.dhiman@slidetech.in");
+				Thread.sleep(3000);
+
+				WebElement g_login_btn1 = wait.until(ExpectedConditions
+						.elementToBeClickable(By.cssSelector("#identifierNext > div > button > span")));
+				;
+				g_login_btn1.click();
+
+				Thread.sleep(3000);
+
+				WebElement g_pass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+						"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")));
+
+				g_pass.sendKeys("Nisha@123");
+
+				Thread.sleep(3000);
+				WebElement g_login_btn2 = wait.until(
+						ExpectedConditions.elementToBeClickable(By.cssSelector("#passwordNext > div > button > span")));
+
+				g_login_btn2.click();
+
+				Thread.sleep(5000);
 			}
-			driver.switchTo().window(currentWindow);
-			Thread.sleep(5000);
-		} 
+		}
+		driver.switchTo().window(currentWindow);
+		Thread.sleep(5000);
+	}
 
 	@Then("user verify the page_viii$")
 	public void user_verify_the_page_viii() {
@@ -137,9 +137,9 @@ public class SLI_search_google_paid_login extends BaseClass {
 		Thread.sleep(4000);
 		WebElement search_field = wait
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='search-input']")));
-		search_field.sendKeys("Mana");
+		search_field.sendKeys("Management");
 		Thread.sleep(1000);
-		search_field.sendKeys("gement");
+		//search_field.sendKeys("gement");
 		Thread.sleep(3000);
 	}
 
@@ -170,6 +170,7 @@ public class SLI_search_google_paid_login extends BaseClass {
 		} catch (NoSuchElementException e) {
 
 		}
+
 	}
 
 }
