1aa1097918789ab7382c96068ff5a307113c7e86
nishaslidetech <nisha.dhiman@slidetech.in>
2022-08-03 15:41:54 +0530

    update captcha

--- src/test/java/Setupclass/TestRunner.java
+++ src/test/java/Setupclass/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "." }, glue = { "StepDefination" },  tags= ("@Google_existing_paid_login"),
+@CucumberOptions(features = { "." }, glue = { "StepDefination" },  tags= ("@facebook_existing_paid_login"),
 
 		plugin = { "pretty", "html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json",
 				"usage:target/usage.jsonx", "junit:target/cucumber.xml" })
